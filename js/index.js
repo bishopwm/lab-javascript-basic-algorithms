a663963df4ee44658395125e437c8d08a38cd1cc
bishopwm <bishopwm@g.cofc.edu>
2020-03-30 15:56:50 -0400

    added conditionals

--- js/index.js
+++ js/index.js
@@ -1,11 +1,21 @@
-// Iteration 1: Names and Input
-let hacker1 = "Will";
+//names and inputs
+
+let hacker1 = "Willllllllll";
 console.log(`The drivers name is ${hacker1}`);
 
 let hacker2 = "Someone else";
 console.log(`The navigators name is ${hacker2}`);
 
-// Iteration 2: Conditionals
+//conditionals
 
+if (hacker1.length > hacker2.length) {
+  console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+}
+else if (hacker1.length < hacker2.length) {
+  console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+}
+else if (hacker1.length === hacker2.length) {
+  console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+};
 
-// Iteration 3: Loops
+//loops

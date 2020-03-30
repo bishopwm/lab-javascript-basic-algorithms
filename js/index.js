b07190b9daa4b757b457ff741725c1eb2918bf13
bishopwm <bishopwm@g.cofc.edu>
2020-03-30 15:34:50 -0400

    names and input

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 // Iteration 1: Names and Input
+let hacker1 = "Will";
+console.log(`The drivers name is ${hacker1}`);
 
+let hacker2 = "Someone else";
+console.log(`The navigators name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 

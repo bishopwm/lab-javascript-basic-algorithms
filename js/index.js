84c2271a624c2de703703c334d228ba1a790194d
bishopwm <bishopwm@g.cofc.edu>
2020-03-30 18:02:17 -0400

    second iteration/loops

--- js/index.js
+++ js/index.js
@@ -19,3 +19,46 @@ else if (hacker1.length === hacker2.length) {
 };
 
 //loops
+
+// //loops
+// console.log('niko'.toUpperCase())
+// //this is niko 
+
+// console.log(hacker1.split(""))
+// //console.log(hacker1)
+//let newHacker1 = hacker1.toUpperCase()
+//let hackerArray=newHacker1.split("").join(" ");
+//console.log(hackerArray);
+
+let newHacker = "";
+
+for (let i=0; i < hacker1.length; i++) {
+newHacker = newHacker + (hacker1.charAt(i)+ " ")
+// newHacker.charAt(i) + " ";
+  
+}
+console.log(newHacker.toUpperCase());
+
+let newWord = newHacker.toUpperCase();
+
+newWord = newWord.split("").reverse().join("");
+
+
+console.log(newWord);
+
+
+// if (hacker1 > hacker2) {
+//     console.log('The driver´s name goes first')
+// } else if (hacker1 < hacker2) {
+//     console.log('The navigator goes first definitely')
+// } else {
+//     console.log('What?! You both got the same name?')
+// } 
+
+// for (let letter in hackerArray) {
+//   console.log(letter.toUpperCase());
+// }
+
+// let nameOrder = "";
+// for (i = hacker1.length -1; i >= 0; i--) {
+//     nameOrder += hacker1[i];
